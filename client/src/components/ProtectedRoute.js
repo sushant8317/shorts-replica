d7c985d4d3405963b6557089ca1f90b77e45d50c
sushant8317 <s.saurav2006@gmail.com>
2025-08-06 19:57:52 +0530

    Update ProtectedRoute.js

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,1 +1,15 @@
+// src/ProtectedRoute.js
+import React from "react";
+import { Navigate, useLocation } from "react-router-dom";
 
+function ProtectedRoute({ children }) {
+  const loggedIn = localStorage.getItem("shorts_loggedin") === "true";
+  const location = useLocation();
+  if (!loggedIn) {
+    // Send user to login page and remember which page they intended
+    return <Navigate to="/login" state={{ from: location.pathname + location.search }} replace />;
+  }
+  return children;
+}
+
+export default ProtectedRoute;
